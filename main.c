8bc8ac89c30834ff0fea1f8b38a2206bc6760561
Theresa <theresa.l1996@gmail.com>
2022-05-12 16:29:08 +0200

    starting point with makefile, basic setup and GPIO functions

--- main.c
+++ main.c
@@ -1,26 +1,18 @@
-#define F_CPU 16000000
+#include "lib/common.h"
+#include "lib/setup.h"
+#include "lib/GPIO.h"
 
-#include <avr/io.h>
-#include <util/delay.h>
-
-
-int main () {
-
-	DDRD |= (1<<7);   // output at PD7, to connect an LED with current-limiting resistor
-
-	PORTD |= (1<<4);  // momentry switch connects PD4 to GND, activate Pullup at PD4
+int main () 
+{
+	setup();
 	
-	
-	while (1) {    // endless loop!
-		
-		if ((PIND & (1<<4)) == 0)     {   // only if the switch is pressed 
+	while (1) 
+	{
 			
-			
-			PORTD |= (1<<7);  // set bit 7 in register PORTD: switch on LED at PD 7
-			_delay_ms(200);
+		PORTD |= (1<<2);  // set bit 7 in register PORTD: switch on LED at PD 7
+		_delay_ms(200);
 
-			PORTD &= ~(1<<7);  // clear bit 7 in register PORTD: switch off LED at PD 7
-			_delay_ms(200);
-		}	
+		PORTD &= ~(1<<2);  // clear bit 7 in register PORTD: switch off LED at PD 7
+		_delay_ms(200);	
 	}
 }	
