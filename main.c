bcc9b884a9e299565b610bbc5621fe848246a499
Theresa <theresa.l1996@gmail.com>
2022-05-12 19:12:44 +0200

    ADC conversion with example code added

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "lib/common.h"
 #include "lib/setup.h"
 #include "lib/GPIO.h"
+#include "lib/ADC.h"
 
 int main () 
 {
@@ -8,11 +9,20 @@ int main ()
 	
 	while (1) 
 	{
-			
-		PORTD |= (1<<2);  // set bit 7 in register PORTD: switch on LED at PD 7
+		uint16_t _POT_value = ADC_read(0);
+		uint16_t _LDR_value = ADC_read(1);
+
+		if (_LDR_value > 0)
+		{
+			PORTD |= (1<<2);  // set bit 7 in register PORTD: switch on LED at PD 7
 		_delay_ms(200);
 
 		PORTD &= ~(1<<2);  // clear bit 7 in register PORTD: switch off LED at PD 7
 		_delay_ms(200);	
+		}			
+		else
+		{
+
+		}
 	}
 }	
