4e66b4d2bd0c5349637bbdb16cb02faae7af3558
Theresa <theresa.l1996@gmail.com>
2022-05-19 22:12:58 +0200

    added atomthreads library, modified MAKEFILE (atomport-asm.o is statically included), added thread templates

---
+++ Atomthreads/lib/threads.h
@@ -0,0 +1,8 @@
+// thread handles polling for the potentiometer value
+void THREAD_getPotentiometerValue(uint32_t param);
+
+// thread handles polling for the LDR value
+void THREAD_getLDRValue(uint32_t param);
+
+// thread adjusts PWM according to potentiometer and LDR
+void THREAD_setPWM(uint32_t param);
---
+++ Atomthreads/src/threads.c
@@ -0,0 +1,20 @@
+#include "../lib/common.h"
+#include "../lib/threads.h"
+
+// thread handles polling for the potentiometer value
+void THREAD_getPotentiometerValue(uint32_t param)
+{
+
+}
+
+// thread handles polling for the LDR value
+void THREAD_getLDRValue(uint32_t param)
+{
+    
+}
+
+// thread adjusts PWM according to potentiometer and LDR
+void THREAD_setPWM(uint32_t param)
+{
+    
+}
